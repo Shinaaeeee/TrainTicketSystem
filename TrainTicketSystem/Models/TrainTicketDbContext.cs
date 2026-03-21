b0881632d4f7acd8d56256e3e8e871ca4651e6c5
Hnah <hanh113004@gmail.com>
2026-03-22 01:46:26 +0700

    improve UI

--- TrainTicketSystem/Models/TrainTicketDbContext.cs
+++ TrainTicketSystem/Models/TrainTicketDbContext.cs
@@ -34,7 +34,12 @@ public partial class TrainTicketDbContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Data Source=NMINH;Initial Catalog=TrainTicketDB;Integrated Security=True;Encrypt=false;TrustServerCertificate=true;");
+        // => optionsBuilder.UseSqlServer(Configuration["ConnectionStrings:MyCnn"]);
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            if (!optionsBuilder.IsConfigured) { optionsBuilder.UseSqlServer(config.GetConnectionString("MyCnn")); }
+        }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- TrainTicketSystem/Program.cs
+++ TrainTicketSystem/Program.cs
@@ -44,8 +44,9 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
-// ---- SignalR Hub endpoint ----
+// ---- SignalR Hub endpoints ----
 app.MapHub<SeatHub>("/seatHub");
+app.MapHub<BookingNotificationHub>("/bookingNotificationHub");
 
 app.Run();
 
