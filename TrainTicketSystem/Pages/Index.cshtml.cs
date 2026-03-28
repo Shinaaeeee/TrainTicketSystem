3e879e05a9652f3e08b81d4e277ef80512e6293f
Hnah <hanh113004@gmail.com>
2026-03-28 16:30:51 +0700

    search sort filter

--- TrainTicketSystem/Pages/Index.cshtml.cs
+++ TrainTicketSystem/Pages/Index.cshtml.cs
@@ -21,6 +21,9 @@ namespace TrainTicketSystem.Pages
         public DateTime? SearchDate { get; set; }
 
         [BindProperty(SupportsGet = true)]
+        public DateTime? ArrivalDate { get; set; }
+
+        [BindProperty(SupportsGet = true)]
         public string FromStation { get; set; }
 
         [BindProperty(SupportsGet = true)]
@@ -29,9 +32,14 @@ namespace TrainTicketSystem.Pages
         [BindProperty(SupportsGet = true)]
         public string SeatType { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string PriceRange { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SortBy { get; set; }
+
         public IActionResult OnGet()
         {
-            
             var role = HttpContext.Session.GetString("Role");
 
             if (role == "Admin")
@@ -49,14 +57,21 @@ namespace TrainTicketSystem.Pages
                             FromStation = r.StartStation,
                             ToStation = r.EndStation,
                             DepartureTime = s.DepartureTime,
+                            ArrivalTime = s.ArrivalTime,
                             Price = s.Price
                         };
-            var now = DateTime.Now;
 
+            var now = DateTime.Now;
             query = query.Where(x => x.DepartureTime >= now);
+
             if (SearchDate != null)
             {
                 query = query.Where(x => x.DepartureTime.Value.Date == SearchDate.Value.Date);
+            }
+
+            if (ArrivalDate != null)
+            {
+                query = query.Where(x => x.ArrivalTime.Value.Date == ArrivalDate.Value.Date);
             }
 
             if (!string.IsNullOrEmpty(FromStation))
@@ -68,6 +83,36 @@ namespace TrainTicketSystem.Pages
             {
                 query = query.Where(x => x.ToStation.Contains(ToStation));
             }
+
+            if (!string.IsNullOrEmpty(PriceRange))
+            {
+                switch (PriceRange)
+                {
+                    case "0-200k":
+                        query = query.Where(x => x.Price >= 0 && x.Price <= 200000);
+                        break;
+                    case "200k-500k":
+                        query = query.Where(x => x.Price > 200000 && x.Price <= 500000);
+                        break;
+                    case "500k-800k":
+                        query = query.Where(x => x.Price > 500000 && x.Price <= 800000);
+                        break;
+                    case "800k-1000k":
+                        query = query.Where(x => x.Price > 800000 && x.Price <= 1000000);
+                        break;
+                }
+            }
+
+            query = SortBy switch
+            {
+                "price_asc" => query.OrderBy(x => x.Price),
+                "price_desc" => query.OrderByDescending(x => x.Price),
+                "departure_asc" => query.OrderBy(x => x.DepartureTime),
+                "departure_desc" => query.OrderByDescending(x => x.DepartureTime),
+                "arrival_asc" => query.OrderBy(x => x.ArrivalTime),
+                "arrival_desc" => query.OrderByDescending(x => x.ArrivalTime),
+                _ => query.OrderBy(x => x.DepartureTime)
+            };
 
             Trains = query.ToList();
 
